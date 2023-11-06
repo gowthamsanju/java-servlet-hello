4f9a87d465b8cdfda4cfe19dee84096049c3fbfa
gowthamsanju <147701948+gowthamsanju@users.noreply.github.com>
2023-11-06 14:24:48 +0530

    Update dockerimgbuild.sh

--- dockerimgbuild.sh
+++ dockerimgbuild.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/e3l3f0q4
 docker build -t java-app .
 docker tag java-app:latest public.ecr.aws/e3l3f0q4/java-app:latest
