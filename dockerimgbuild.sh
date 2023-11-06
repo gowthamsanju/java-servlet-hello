16e4c08925fbc2fda3e2d71017ffb0d156171a70
gowthamsanju <147701948+gowthamsanju@users.noreply.github.com>
2023-11-06 16:17:42 +0530

    Update dockerimgbuild.sh

--- dockerimgbuild.sh
+++ dockerimgbuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker-compose down
-docker rm -f $(docker ps -a -q)
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
 docker rmi $(docker images -q)
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/e3l3f0q4
 docker build -t java-app .
